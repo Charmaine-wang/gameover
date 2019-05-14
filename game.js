7f3a6585fb71976c6a324b088281ae12648ea5be
erikarvidsson <erik.arvidsson.89@gmail.com>
2019-05-14 13:55:09 +0200

    started adding palyer

--- game.js
+++ game.js
@@ -1,8 +1,24 @@
+// import Charmaine from './assets/dude.png';
+const gameState = {}
+
+
+function preload(){
+    this.load.spritesheet('dude',
+        './assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    )
+    // let dude = new Charmaine(this.gameState, 0, 0);
+    // dude.loadImage();
+};
+
+
+
 function create() {
     // make player
 
     //cursor event
     gameState.cursor = this.input.keyboard.createCursorKeys();
+    gameState.dude = this.add.sprite(16, 284, 'dude');
 }
 
 function update(){
@@ -11,28 +27,41 @@ function update(){
 
     //move right
     if(gameState.cursor.right.isDown){
-        gameState.codey.x +=5;
+        gameState.dude.x += 5;
     }
 
     //move left
     if (gameState.cursor.left.isDown){
-        gameState.codey.x -= 5;
+        gameState.dude.x -= 5;
     }
 
     //move up
     if (gameState.cursor.up.isDown) {
-        gameState.codey.x -= 5;
+        gameState.dude.y -= 5;
+    }
+    if (gameState.cursor.down.isDown) {
+        gameState.dude.y += 5;
     }
 
 }
 
 const config = {
-
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 600,
+    backgroundColor: "#5f2a55",
+    // physics: {
+    //     default: 'arcade',
+    //     arcade: {
+    //     gravity: { y: 300 },
+    //     debug: false
+    //     },
     //gets all the functions
     scene: {
         create,
         update
     }
 }
+// }
 
 const game = new Phaser.Game(config);
