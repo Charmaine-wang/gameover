434156da4813922004ceb10cad4b390f9912be9b
erikarvidsson <erik.arvidsson.89@gmail.com>
2019-05-16 10:34:07 +0200

    try adding camera

--- game.js
+++ game.js
@@ -20,7 +20,8 @@ const config = {
     scene: {
         preload,
         create,
-        update
+        update,
+        render
     }
 }
 
@@ -36,55 +37,76 @@ function preload(){
                     // this.load.image('bomb', '/assets/bomb.png');
                     this.load.spritesheet(
                       "dude",
-                      "/assets/dude.png",
+                      "/assets/dude2.png",
                       { frameWidth: 32, frameHeight: 48 }
                     );
-
                   };
 
 
 function create() {
-    // make map
-    this.add.image(0, 0, "sky").setOrigin(0, 0);
-    this.add.image(500, 400, "star");
+    if(gameState) {
+
+        // make map
+        this.add
+        .image(0, 0, "sky")
+        .setOrigin(0, 0);
+        this.add.image(500, 400, "star");
+
+        //make ground
+        platforms = this.physics.add.staticGroup();
+        
+        platforms
+        .create(400, 568, "ground")
+        .setScale(2)
+        .refreshBody();
+        platforms.create(600, 400, "ground");
+        platforms.create(50, 250, "ground");
+        platforms.create(750, 220, "ground");
+        
+        // make player
+        dude = this.physics.add.sprite(16, 284, "dude");
+        dude.setBounce(0.4);
+        dude.setCollideWorldBounds(true);
+        this.physics.add.collider(dude, platforms);
+
+        dude.body.fixedRotation = true;
 
 
-    //make ground
-    platforms = this.physics.add.staticGroup();
+        // this.bg.setScrollFactor(0);
 
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-
-    // make player
-    gameState.dude = this.physics.add.sprite(16, 284, 'dude');
-    gameState.dude.setBounce(0.4);
-    gameState.dude.setCollideWorldBounds(true);
-    this.physics.add.collider(gameState.dude, platforms);
-
-    //cursor event
-    gameState.cursor = this.input.keyboard.createCursorKeys();
-
-}
-
-function update(){
-    //do we need thois?
-    //if(gameState.cursor.down.isDown){
-
-    //move right
-    if(gameState.cursor.right.isDown){
-        gameState.dude.x += 5;
+        gameState.camera.follow(
+          dude,
+          Phaser.Camera.FOLLOW_LOCKON,
+          0.1,
+          0.1
+        );
+        // gameState.dude.anchor.setTo(0.5, 0.5);                 
+        // gameState.camera.bounds = (608, 400);
+        // Try to add Camer that follws player
+        // this.cameras.main.startFollow(this.dude);
+        // this.cameras.main.roundPixels = true;
+        //cursor event
+            
+            gameState.cursor = this.input.keyboard.createCursorKeys();
+        } 
+                
     }
-
-    //move left
-    if (gameState.cursor.left.isDown){
-        gameState.dude.x -= 5;
+                function update(){
+                    //do we need thois?
+                    //if(gameState.cursor.down.isDown){ 
+                        //move right
+                        if(gameState.cursor.right.isDown){
+                            dude.x += 5;
+                        }
+                        
+                        //move left
+                        if (gameState.cursor.left.isDown){
+        dude.x -= 5;
     }
 
     //move up
     if (gameState.cursor.up.isDown) {
-        gameState.dude.y -= 5;
+        dude.y -= 5;
     }
     // if (gameState.cursor.down.isDown) {
     //     gameState.dude.y += 5;
@@ -99,6 +121,11 @@ function update(){
 
 
 
+}
+
+function render() {
+  game.debug.cameraInfo(game.camera, 32, 32);
+  game.debug.spriteCoords(dude, 32, 500);
 }
 
 
